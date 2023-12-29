2cdccb727e3b464d82be2a15931304d4c09a4639
Ayattt-io <akrabullah@gmail.com>
2023-12-29 21:07:26 +0700

    update jaccard code

--- jaccard.py
+++ jaccard.py
@@ -11,28 +11,35 @@ def jakar(set_a, set_b):
     return intersection / union if union != 0 else 0.0
 
 
-benar = ["tolong", "matikan", "lampu"]
+benar = ["tolong", "matikan", "lampu", "cook"]
 kalimat = input("Masukkan kalimat : ")
 berkata = kalimat.split()
-similarity_threshold = 0.5
+# similarity_threshold = 0.5
 kalimat_benar = []
 
 print(type(berkata))
 
 for kata in berkata:
-    kata_set = set(kata)
+    # kata_set = set(kata)
     kondisi = False
+    kata_set = set(kata)
+    highest_similarity = 0.0
+    most_similar_word = kata  # Default to the original word
     for benar_kata in benar:
         benar_set = set(benar_kata)
         similarity = jakar(kata_set, benar_set)
-        if similarity > similarity_threshold:
-            kalimat_benar.append(benar_kata)
+        # if similarity >= similarity_threshold:
+
+        if similarity > highest_similarity:
             kondisi = True
+            highest_similarity = similarity
+            most_similar_word = benar_kata
             print("Hasil kesamaan Jaccard antara kalimat input '" + kata +
-                  "' dan kalimat benar '" + benar_kata + "' adalah " + str(similarity))
+                  "' dan kalimat benar '" + most_similar_word + "' adalah " + str(similarity))
             print(f" kata_set : {kata_set}")
             print(f" benar_kata : {benar_kata}")
-            break
+
+    kalimat_benar.append(most_similar_word)
 
     if not kondisi:
         kalimat_benar.append(kata)
