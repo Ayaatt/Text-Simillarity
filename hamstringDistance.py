4bbf48df32b6282481cfae07c2019e139d61a288
Ayattt-io <akrabullah@gmail.com>
2023-12-29 17:54:42 +0700

    add hamming distance

---
+++ hamstringDistance.py
@@ -0,0 +1,15 @@
+def hamming_distance(str1, str2):
+    # Menghitung Hamming Distance antara dua string
+    if len(str1) != len(str2):
+        raise ValueError("String harus sama panjangnya")
+
+    distance = 0
+    for ch1, ch2 in zip(str1, str2):
+        if ch1 != ch2:
+            distance += 1
+    return distance
+
+
+# Contoh penggunaan
+distance = hamming_distance("karat", "karetnya")
+print("Hamming Distance:", distance)
