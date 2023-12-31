f24edf4334635e1e7947c2f17e5982f02c96852a
Ayattt-io <akrabullah@gmail.com>
2023-12-31 10:51:50 +0700

    rename needleman file

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import jaccard
+import cosine
+import damerauLevi
+import diceCoef
+import euclidienDistance
+import jaroDistance
+import jaroWinkler
+import levenshteinD
+import manhattan
+import nGram
+import smithWaterman
+import hamstringDistance
+import longestCommon
 
 jaccard.jekar()
