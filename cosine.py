2e00d6270dc837fde28f21ea1b0458c31f507e53
Ayattt-io <akrabullah@gmail.com>
2023-12-31 12:42:23 +0700

    simplify the code

--- cosine.py
+++ cosine.py
@@ -5,7 +5,7 @@ from collections import Counter
 def text_to_vector(text):
     # Mengonversi teks menjadi vektor frekuensi kata
     word_counts = Counter(text.split())
-    print(f"Word Counts : {word_counts}")
+    # print(f"Word Counts : {word_counts}")
     return word_counts
 
 
@@ -13,14 +13,14 @@ def cosine_similarity(vec1, vec2):
     # Menghitung dot product antara dua vektor
     intersection = set(vec1.keys()) & set(vec2.keys())
     dot_product = sum([vec1[x] * vec2[x] for x in intersection])
-    print(f"intersection : {intersection}")
-    print(f"dot product : {dot_product}")
+    # print(f"intersection : {intersection}")
+    # print(f"dot product : {dot_product}")
 
     # Menghitung magnitude masing-masing vektor
     magnitude1 = math.sqrt(sum([val**2 for val in vec1.values()]))
     magnitude2 = math.sqrt(sum([val**2 for val in vec2.values()]))
-    print(f"Magnitude 1 : {magnitude1}")
-    print(f"Magnitude 2 : {magnitude2}")
+    # print(f"Magnitude 1 : {magnitude1}")
+    # print(f"Magnitude 2 : {magnitude2}")
 
     # Menghitung dan mengembalikan kesamaan kosinus
     if not magnitude1 or not magnitude2:
@@ -29,15 +29,15 @@ def cosine_similarity(vec1, vec2):
         return dot_product / (magnitude1 * magnitude2)
 
 
-# Contoh penggunaan
-text1 = "I I enjoy reading about artificial intelligence"
-text2 = "I enjoy reasing aboyt artifscioal imtekligemce"
+# # Contoh penggunaan
+# text1 = "I I enjoy reading about artificial intelligence"
+# text2 = "I enjoy reasing aboyt artifscioal imtekligemce"
 
-vector1 = text_to_vector(text1)
-vector2 = text_to_vector(text2)
-print(f"Vektor 1 : {vector1}")
-print(f"Vektor 1 : {vector2}")
+# vector1 = text_to_vector(text1)
+# vector2 = text_to_vector(text2)
+# print(f"Vektor 1 : {vector1}")
+# print(f"Vektor 1 : {vector2}")
 
 
-similarity = cosine_similarity(vector1, vector2)
-print("Cosine Similarity:", similarity)
+# similarity = cosine_similarity(vector1, vector2)
+# print("Cosine Similarity:", similarity)
--- main.py
+++ main.py
@@ -11,5 +11,29 @@ import nGram
 import smithWaterman
 import hamstringDistance
 import longestCommon
+import needlemanWunsch
+import time
 
-jaccard.jekar()
+
+def tokenize(query):
+    set_query_input = set_query_input.lower().split()
+    set_query_input = set(query)
+    return set_query_input
+
+
+# jaccard.jekar()
+dictionary = ["on", "off", "fan", "tv", "television", "PC", "lamp"]
+user_query = input("Masukkan kalimat : ")
+user_query = user_query.lower()  # Convert query to lowercase
+for kata in user_query:
+    tokens = tokenize(user_query)  # Tokenize the user query string
+    start_time = time.time()  # Start measuring time
+    corrected_tokens = []  # create a new array of text
+    for token in tokens:
+        if token in [".", ",", "!", "?", ":", ";", "the"]:
+            corrected_tokens.append(token)
+            continue
+# result = text_correction(token)
+corrected_tokens.append(token)
+elapsed_time = time.time() - start_time
+corrected_sentence = " ".join(corrected_tokens)
