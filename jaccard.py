b32b2d2b882342ddde67987e7253334d40a5e179
Ayattt-io <akrabullah@gmail.com>
2023-12-31 10:49:59 +0700

    add a function to run jaccard

--- jaccard.py
+++ jaccard.py
@@ -19,31 +19,33 @@ kalimat_benar = []
 
 print(type(berkata))
 
-for kata in berkata:
-    # kata_set = set(kata)
-    kondisi = False
-    kata_set = set(kata)
-    highest_similarity = 0.0
-    most_similar_word = kata  # Default to the original word
-    for benar_kata in benar:
-        benar_set = set(benar_kata)
-        similarity = jakar(kata_set, benar_set)
-        # if similarity >= similarity_threshold:
 
-        if similarity > highest_similarity:
-            kondisi = True
-            highest_similarity = similarity
-            most_similar_word = benar_kata
-            print("Hasil kesamaan Jaccard antara kalimat input '" + kata +
-                  "' dan kalimat benar '" + most_similar_word + "' adalah " + str(similarity))
-            print(f" kata_set : {kata_set}")
-            print(f" benar_kata : {benar_kata}")
+def jekar():
+    for kata in berkata:
+        # kata_set = set(kata)
+        kondisi = False
+        kata_set = set(kata)
+        highest_similarity = 0.0
+        most_similar_word = kata  # Default to the original word
+        for benar_kata in benar:
+            benar_set = set(benar_kata)
+            similarity = jakar(kata_set, benar_set)
+            # if similarity >= similarity_threshold:
 
-    kalimat_benar.append(most_similar_word)
+            if similarity > highest_similarity:
+                kondisi = True
+                highest_similarity = similarity
+                most_similar_word = benar_kata
+                print("Hasil kesamaan Jaccard antara kalimat input '" + kata +
+                      "' dan kalimat benar '" + most_similar_word + "' adalah " + str(similarity))
+                print(f" kata_set : {kata_set}")
+                print(f" benar_kata : {benar_kata}")
 
-    if not kondisi:
-        kalimat_benar.append(kata)
+        kalimat_benar.append(most_similar_word)
 
-kalimatnya = ' '.join(kalimat_benar)
-print(f" HASILNYA : {kalimatnya}")
-print(berkata)
+        if not kondisi:
+            kalimat_benar.append(kata)
+
+    kalimatnya = ' '.join(kalimat_benar)
+    print(f" HASILNYA : {kalimatnya}")
+    print(berkata)
